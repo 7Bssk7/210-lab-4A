77dd10201675e1500e03db4505a131d746c55229
7Bssk7 <arhip.fin3@gmail.com>
2025-09-07 20:01:49 -0700

    Initial commit

---
+++ 210-lab-4A.cpp
@@ -0,0 +1,13 @@
+// COMSC-210 | Lab 4A | Arkhip Finski
+// IDE used : Visual Studio
+
+#include<iostream>
+#include<vector>
+#include<random>
+#include<iomanip>
+using namespace std;
+
+int main(){
+
+    return 0;
+}
